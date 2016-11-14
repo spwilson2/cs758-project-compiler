419c6ed436f3759b124004480c86ed6f5063edd4
Sean Wilson <spwilson27@gmail.com>
2016-11-14 13:32:28 -0600

    Adds a stage2 builder

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ unset TEST_FINAL_COMPILER
 # Comment out to avoid testing of the compiler.
 TEST_FINAL_COMPILER="y"
 
-export GO_COMPILER="`pwd`/go-compiler"
-export GOROOT_BOOTSTRAP="`pwd`/bootstrap-compiler"
+BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export GO_COMPILER="$BUILD_DIR/go-compiler"
+export GOROOT_BOOTSTRAP="$BUILD_DIR/bootstrap-compiler"
+export STAGE2_PFX="-stage2"
 
 export GO_VERSION="release-branch.go1.7"
 export GO_SOURCE="https://github.com/golang/go"
 
-BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ ! -d "$GOROOT_BOOTSTRAP" ] 
 then 
     curl https://storage.googleapis.com/golang/go1.4-bootstrap-20161024.tar.gz | tar xz 
@@ -22,11 +24,19 @@ then
     cd -
 fi
 
+if [ ! -d "${GO_COMPILER}${STAGE2_PFX}" ]
+then
+    git clone -b "$GO_VERSION" --single-branch "$GO_SOURCE" "${GO_COMPILER}${STAGE2_PFX}"
+    cd "${GO_COMPILER}${STAGE2_PFX}/src"
+	./make.bash
+	cd -
+fi
+
 if [ ! -d "$GO_COMPILER" ]
 then
     git clone -b "$GO_VERSION" --single-branch "$GO_SOURCE" "$GO_COMPILER"
     cd "$GO_COMPILER/src"
-	./make.bash
+	GOROOT_BOOTSTRAP="${GO_COMPILER}${STAGE2_PFX}" GOROOT="${GO_COMPILER}${STAGE2_PFX}" PATH="${GO_COMPILER}${STAGE2_PFX}/bin:$PATH" ./make.bash
 	cd -
 fi
 
