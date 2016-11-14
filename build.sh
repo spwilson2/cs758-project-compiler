b53cb8d574bf6856f7a04cb1bcbe73c4d34db56e
Sean Wilson <spwilson27@gmail.com>
2016-11-14 01:02:21 -0600

    Remove CGO support in bootstrapper to fix issues on multiple hosts

--- build.sh
+++ build.sh
@@ -14,7 +14,9 @@ then
     curl https://storage.googleapis.com/golang/go1.4-bootstrap-20161024.tar.gz | tar xz 
     mv go "$GOROOT_BOOTSTRAP"
     cd "$GOROOT_BOOTSTRAP/src"
-    ./make.bash
+	# We build without CGO_ENABLED since it's not needed for a bootstrapper
+	# and has a tendency to break compilation on some hosts
+    CGO_ENABLED=0 ./make.bash
     cd -
 fi
 
@@ -22,7 +24,8 @@ if [ ! -d "$GO_COMPILER" ]
 then
     git clone https://github.com/golang/go "$GO_COMPILER"
     cd "$GO_COMPILER/src"
-        ./make.bash
+	./make.bash
+	cd -
 fi
 
 export GOROOT="$GO_COMPILER"
