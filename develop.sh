ed06617f637f45451f150903b67d8f4aa9b9ccc3
Sean Wilson <spwilson27@gmail.com>
2016-11-21 20:02:52 -0600

    Add our modded compiler to the mix

--- develop.sh
+++ develop.sh
@@ -4,4 +4,11 @@ GO_COMPILER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 make -C "$GO_COMPILER_DIR" all
 
-export PATH="$GO_COMPILER_DIR/vanilla-compiler/bin:$PATH"
+if [ "$1" == "devel" ]
+then
+	echo "Using modded go compiler!"
+	export PATH="$GO_COMPILER_DIR/modded-compiler/bin:$PATH"
+else
+	echo "Using vanilla go compiler."
+	export PATH="$GO_COMPILER_DIR/vanilla-compiler/bin:$PATH"
+fi
